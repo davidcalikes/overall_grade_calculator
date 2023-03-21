4e0ac794272e9d6c06cd70bb8eb7a95fc501e804
David Calikes <davidcalikes@gmail.com>
2023-03-21 12:35:16 +0000

    feat: Add constant variables to script.js

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+// Project One Scores //
+const pOnePass = 32
+const pOneMerit = 48
+const pOneDist = 64
+// Project Two Scores //
+const pTwoPass = 32
+const pTwoMerit = 48
+const pTwoDist = 64
+// Project Three Scores //
+const pThreePass = 32
+const pThreeMerit = 48
+const pThreeDist = 64
+// Project Four Scores //
+const pFourPass = 64
+const pFourMerit = 96
+const pFourDist = 128
+// Project Five Scores //
+const pFivePass = 80
+const pFiveMerit = 120
+const pFiveDist = 160
+// Overall Course Grades //
+
+const overallPass = 'Pass';
+const overallMerit = 'Merit';
+const overallDist = 'Distinction';
